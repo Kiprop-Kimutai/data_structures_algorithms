f687fb1099cfac148aa9cbeb10662502e85c5cde
Kiprop-Kimutai <jonahkiprop34@gmail.com>
2019-06-14 15:10:48 +0300

    #js implementation of bubble sort algorithm

--- sort/bubble-sort.js
+++ sort/bubble-sort.js
@@ -4,13 +4,19 @@
 
 function bubbleSort(arr) {
   var i,j,temp;
+  let swapped = false;
   for(i=0;i<arr.length;i++) {
     for(j=i+1;j<arr.length;j++) {
       if(arr[i] > arr[j]) {
         temp = arr[j];
         arr[j] = arr[i];
         arr[i] = temp;
+        swapped = true;
       }
+    }
+    if(!swapped) {
+      //list is sorted
+      break;
     }
   }
   return arr;
